062e743030636fdd5a19f4bf110c974244524a60
Igor Jevremović <ijevremovic@yahoo.com>
2024-09-30 10:27:43 +0200

    Add files via upload

--- JS/vjezba67-react/src/App.jsx
+++ JS/vjezba67-react/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const App = () => {
   const initialValues = { user: "", email: "", pass: "" };
@@ -6,34 +7,76 @@ const App = () => {
   const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
 
+  const form = useRef();
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
-    console.log(formValues);
+  };
+
+  const handleFormReset = () => {
+    setFormValues(initialValues);
+    setFormErrors({});
+    setIsSubmit(true);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormErrors(validate(formValues));
     setIsSubmit(true);
+
+    if (Object.keys(validate(formValues)).length === 0) {
+      emailjs
+        .sendForm("service_6dh7lkj", "template_01hwhu9", form.current, {
+          publicKey: "WEmu56w5cCsRuO63q",
+        })
+        .then(
+          () => {
+            handleFormReset();
+            console.log("Uspjeh");
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    }
   };
 
   const validate = (v) => {
     const errors = {};
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (!v.user) {
       errors.user = "Korisničko ime nije ispravno upisano";
     }
     if (!v.email) {
-      errors.email = "E-mail nije ispravno upisan";
+      errors.email = "E-mail polje ne može ostati prazno";
+    } else if (!regex.test(v.email)) {
+      errors.email = "Niste unijeli ispravnu e-mail adresu";
     }
     if (!v.pass) {
-      errors.pass = "Lozinka nije ispravno upisana";
+      errors.pass = "Ovo polje ne može ostati prazno";
+    } else if (v.pass.length < 6) {
+      errors.pass = "Lozinka mora imati više od 6 znakova";
     }
     return errors;
   };
 
   return (
     <div className="container">
-      <form onSubmit={handleSubmit}>
+      {Object.keys(formErrors).length === 0 && isSubmit ? (
+        <div className="title">Uspjeh</div>
+      ) : (
+        <div className="title">Ispunite formu</div>
+      )}
+
+      <form ref={form} onSubmit={handleSubmit}>
         <h1>Login forma</h1>
         <hr />
         <div className="form">
@@ -48,6 +91,7 @@ const App = () => {
               onChange={handleChange}
             />
           </div>
+          <p>{formErrors.user}</p>
           <div className="field">
             <label htmlFor="email">Email</label>
             <input
@@ -59,6 +103,7 @@ const App = () => {
               onChange={handleChange}
             />
           </div>
+          <p>{formErrors.email}</p>
           <div className="field">
             <label htmlFor="password">Password</label>
             <input
@@ -70,6 +115,7 @@ const App = () => {
               onChange={handleChange}
             />
           </div>
+          <p>{formErrors.pass}</p>
           <button className="btn">Submit</button>
         </div>
       </form>
